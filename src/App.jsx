552ccfc239b1e6aaec5f995b927412c791227e5e
railson <railsonleite@gmail.com>
2026-06-24 22:01:02 -0300

    novas rotas app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Login from './pages/Login'
 import Marketplace from './pages/Marketplace'
 import AdminPanel from './pages/PainelAdministrativo'
 import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Diagnostico from './pages/Diagnostico'
+import Mentorias from './pages/Mentorias'
+import Trilas from './pages/Trilhas'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<Register/>} />
       <Route path="/marketplace" element={<Marketplace />} />
       <Route path="/admin" element={<AdminPanel />} />
+      <Route path="/Diagnostico" element={<Diagnostico/>}/>
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
