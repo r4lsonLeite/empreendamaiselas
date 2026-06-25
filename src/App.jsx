d00574e06480265b901d46339c0caa4dd58a47ba
Marcos Felipe <marcosfe54321@gmail.com>
2026-06-25 11:27:48 -0300

    ajuste nas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
-import Marketplace from './pages/Marketplace'
-import AdminPanel from './pages/PainelAdministrativo'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Diagnostico from './pages/Diagnostico'
+import Marketplace from './pages/Marketplace'
 import Mentorias from './pages/Mentorias'
-import Trilas from './pages/Trilhas'
+import Trilhas from './pages/Trilhas'
+import PainelAdministrativoVisaoGeral from './pages/PainelAdministrativoVisaoGeral'
+import PainelEmpreendedora from './pages/PainelEmpreendedora'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register/>} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/diagnostico" element={<Diagnostico />} />
       <Route path="/marketplace" element={<Marketplace />} />
-      <Route path="/admin" element={<AdminPanel />} />
-      <Route path="/Diagnostico" element={<Diagnostico/>}/>
+      <Route path="/mentorias" element={<Mentorias />} />
+      <Route path="/trilhas" element={<Trilhas />} />
+      <Route path="/admin" element={<PainelAdministrativoVisaoGeral />} />
+      <Route path="/painel-empreendedora" element={<PainelEmpreendedora />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
